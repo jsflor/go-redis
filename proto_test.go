dcd8c19aca4e5653b0b430aef4cf03c00dcbd69d
Sebastian Flor <sebastian.flor@atheneum.ai>
2024-06-27 13:12:13 +0200

    feat: add proto

---
+++ proto_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestParseCommand(t *testing.T) {
+	raw := "*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"
+
+	cmd, err := parseCommand(raw)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println(cmd)
+}
